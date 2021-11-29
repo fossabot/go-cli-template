fae62cbacf78b19410767a348a4bc5f122107f86
Gabe Hoban <gabehoban@posteo.de>
2021-11-29 02:39:19 -0500

    init

--- app.go
+++ app.go
@@ -1,30 +1,9 @@
 package main
 
 import (
-	"os"
-	"runtime"
-
-	"github.com/gabehoban/go-cli-template/cmd"
-	"github.com/gabehoban/go-cli-template/core"
-	"github.com/urfave/cli/v2"
+  "github.com/gabehoban/go-cli-template/cmd"
 )
 
-func init() {
-	runtime.GOMAXPROCS(runtime.NumCPU())
-}
-
 func main() {
-	app := cli.App{
-		Name:                 core.AppConfig.GetString("app", "name"),
-		Usage:                core.AppConfig.GetString("app", "desc"),
-		Version:              core.AppConfig.GetString("app", "version"),
-		Author:               core.AppConfig.GetString("app", "author"),
-		Email:                core.AppConfig.GetString("app", "email"),
-		EnableBashCompletion: true,
-		Commands: []cli.Command{
-			cmd.Hello,
-		},
-	}
-
-	app.Run(os.Args)
+  cmd.Execute()
 }
--- core/init.go
+++
@@ -1,8 +0,0 @@
-package core
-
-// AppConfig is a wrapper for the application configuration
-var AppConfig config
-
-func init() {
-	AppConfig.InitConfig()
-}
